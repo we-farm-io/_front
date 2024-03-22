7505dcfa837bb65a0d37f29a5bafeb2eecdffb4c
AchouakDaamache <reyhana13012005@gmail.com>
2024-03-22 21:41:41 +0100

    worked on the disease detection page

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:smart_farm/drawer.dart';
-import 'package:smart_farm/onboarding.dart';
-//import 'package:smart_farm/onboarding.dart';
-import 'package:smart_farm/views/agro_insight.dart';
-import 'package:smart_farm/views/home_page.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:smart_farm/drawer.dart';
+// import 'package:smart_farm/onboarding.dart';
 import 'package:smart_farm/views/plant_doc.dart';
-import 'package:smart_farm/views/store.dart';
-import 'package:smart_farm/views/weather.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+//import 'package:smart_farm/onboarding.dart';
+// import 'package:smart_farm/views/agro_insight.dart';
+// import 'package:smart_farm/views/home_page.dart';
+// import 'package:smart_farm/views/plant_doc.dart';
+// import 'package:smart_farm/views/store.dart';
+// import 'package:smart_farm/views/weather.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 import 'package:smart_farm/widgets/nav_bar.dart';
 
 void main() => runApp(const MyApp());
@@ -20,28 +21,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'AgriTech',
-      home: FutureBuilder(
-        future:
-            SharedPreferences.getInstance(), // Get SharedPreferences instance
-        builder: (context, AsyncSnapshot<SharedPreferences> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            // Show a loading indicator while waiting for SharedPreferences to be initialized
-            return const CircularProgressIndicator();
-          } else {
-            // Check if the onboarding has been completed before
-            bool onboardingCompleted = true;
-            // snapshot.data?.getBool('onboardingCompleted') ?? false;
+      home: 
+      PlantDoc(),
+      // FutureBuilder(
+      //   future:
+      //       SharedPreferences.getInstance(), // Get SharedPreferences instance
+      //   builder: (context, AsyncSnapshot<SharedPreferences> snapshot) {
+      //     if (snapshot.connectionState == ConnectionState.waiting) {
+      //       // Show a loading indicator while waiting for SharedPreferences to be initialized
+      //       return const CircularProgressIndicator();
+      //     } else {
+      //       // Check if the onboarding has been completed before
+      //       bool onboardingCompleted = true;
+      //       // snapshot.data?.getBool('onboardingCompleted') ?? false;
 
-            // Decide whether to show onboarding or directly launch homepage
-            return onboardingCompleted
-                ? const NavNotifier()
-                : const OnBoarding(); // onboarding is disabled for now
-          }
-        },
-      ),
+      //       // Decide whether to show onboarding or directly launch homepage
+      //       return onboardingCompleted
+      //           ? const NavNotifier()
+      //           : const OnBoarding(); // onboarding is disabled for now
+      //     }
+      //   },
+      // ),
     );
   }
 }
